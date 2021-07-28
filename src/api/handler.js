b80fa7a83d279053240baad248362b9653768c5e
sdaudishvili <sdaudishvili@gmail.com>
2021-07-28 14:47:12 +0400

    feat :error handler

--- src/api/handler.js
+++ src/api/handler.js
@@ -1,11 +1,20 @@
+import { messages } from '@/utils/messages';
+
 export const handleSuccess = (resolve) => (response) => {
   return resolve(response.data);
 };
 
 export const handleError = (reject) => (error) => {
-  if (error.response && error.response.data) {
-    reject(error.response.data);
-  } else {
-    reject(error);
+  if (error.response) {
+    if (error.response.data) {
+      return reject(error.response.data);
+    }
+    const statusCode = error.response.status;
+    if (statusCode === 401) {
+      return reject(messages.UnAuthorizedRequest);
+    }
+
+    return reject(messages.ErrorOccured);
   }
+  return reject(error);
 };
--- src/utils/messages/index.js
+++ src/utils/messages/index.js
@@ -1,5 +1,7 @@
 export const messages = {
   UpdateSuccess: 'Successfully updated!',
   CreateSuccess: 'Successfully created!',
-  DeleteSuccess: 'Successfully deleted!'
+  DeleteSuccess: 'Successfully deleted!',
+  UnAuthorizedRequest: 'Please Login in to continue!',
+  ErrorOccured: 'Error occured!'
 };
